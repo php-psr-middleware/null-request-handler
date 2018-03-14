79299f177d39059e5306dfde9dc27709bf758c91
Praveen Saxena <saxep01@gmail.com>
2018-03-14 02:22:25 -0500

    add handle method

--- src/NullRequestHandler.php
+++ src/NullRequestHandler.php
@@ -3,7 +3,14 @@ declare(strict_types = 1);
 
 namespace PsrMiddlewares;
 
-class NullRequestHandler
-{
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
+class NullRequestHandler implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+
+    }
 }
